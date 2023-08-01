b939634204264fbb12971e9cce39941c653c2486
FransergioDev <fransergio.dev@gmail.com>
2023-08-01 16:14:45 -0300

    finish test

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -5,8 +5,17 @@ describe('login', () => {
     const mockAlert = jest.fn()
     window.alert = mockAlert
 
+    afterEach(() => {
+        mockAlert.mockClear();
+    });
+
     it('Deve exibir um alert com boas vindas', () => {
         login()
-        expect(mockAlert).toHaveBeenCalledWith('Bem vinda!')
+        expect(mockAlert).toHaveBeenCalledWith('Bem vindo(a)!')
+    })
+    it('Deve exibir um alert com boas vindas com o nome passado por parametro', () => {
+        const name = 'Fransérgio';
+        login(name)
+        expect(mockAlert).toHaveBeenCalledWith(`Bem vindo(a) ${name}!`)
     })
 })
